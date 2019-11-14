0b0ad5c82c9bd07e9e54f6c227d8cdba63c02a1f
berezkin88 <berezkin05@gmail.com>
2019-11-14 19:00:05 +0200

    add APIs for mocking each back endpoint, add files to gitignore

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const path = require("path");
 const router = express.Router();
 
 // custom APIs
-router.get("/logout", (request, response) => {
-    response.set({status: "Logged out"}).redirect("back");
+router.get("/logout", async (request, response) => {
+    response.set({ status: "Logged out" }).redirect("back");
 });
 
-router.get("/v1/aspsps", (request, response) => {
+router.get("/v1/aspsps/", (request, response) => {
     response.set("X-Total-Elements", "22");
-    response.sendFile(path.join(__dirname + "/resources/responses/search.json"));
+    if (!request.query.page) {
+        response.sendFile(path.join(__dirname + "/resources/responses/search0x.json"));
+    } else if (request.query.page === "1") {
+        response.sendFile(path.join(__dirname + "/resources/responses/search1x.json"));
+    } else if (request.query.page === "2") {
+        response.sendFile(path.join(__dirname + "/resources/responses/search2x.json"));
+    } else {
+        response.sendFile(path.join(__dirname + "/resources/responses/searchEmpty.json"));
+    }
 });
 
 router.get("/v1/aspsps/csv/download", (request, response) => {
@@ -19,12 +27,60 @@ router.get("/v1/aspsps/csv/download", (request, response) => {
 });
 
 router.post("/v1/aspsps/csv/merge", (request, response) => {
-    response.status(200).end();
+    response.sendStatus(200);
 });
 
 router.post("/v1/aspsps/csv/upload", (request, response) => {
-    response.status(200).end();
+    response.sendStatus(200);
 });
+
+router.post("/v1/aspsps", (request, response) => {
+    if (!request.body) {
+        response.sendStatus(400);
+    } else {
+        response.sendStatus(201);
+    }
+});
+
+router.put("/v1/aspsps", (request, response) => {
+    if (!request.body) {
+        response.sendStatus(400);
+    } else {
+        response.sendStatus(202);
+    }
+});
+
+router.delete("/v1/aspsps/:id", (request, response) => {
+    if (!request.params.id) {
+        response.sendStatus(400);
+    } else {
+        response.sendStatus(204);
+    }
+});
+
+// router.post("/v1/aspsps/csv/merge", (request, response) => {
+//     response.sendStatus(403);
+// });
+
+// router.post("/v1/aspsps/csv/upload", (request, response) => {
+//     response.sendStatus(403);
+// });
+
+// router.post("/v1/aspsps", (request, response) => {
+//     response.sendStatus(403);
+// });
+
+// router.put("/v1/aspsps", (request, response) => {
+//     response.sendStatus(403);
+// });
+
+// router.delete("/v1/aspsps/:id", (request, response) => {
+//     if (!request.params.id) {
+//         response.sendStatus(400);
+//     } else {
+//         response.sendStatus(403);
+//     }
+// });
 
 // router for static libraries e.g. css, js, etc.
 app.use(express.static(__dirname + '/resources/static'));
@@ -32,9 +88,9 @@ app.use(express.static(__dirname + '/resources/static'));
 app.use("/", router);
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log(`Error occured: ${err}`);
-  }
+    if (err) {
+        return console.log(`Error occured: ${err}`);
+    }
 
-  console.log(`server is listening on ${port}`);
+    console.log(`server is listening on ${port}`);
 });
